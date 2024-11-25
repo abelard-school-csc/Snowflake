8af1a5ace36d94fbe631fdcc233a67527402a1bf
Abelard School Computer Science Club <amuratov@abelardschool.org>
2024-11-25 12:39:57 -0500

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,50 +1,37 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def fractal_polygon(order, sides=3, length=1.0):
-    angles = np.linspace(0, 2 * np.pi, sides + 1)
-    points = np.array([[np.cos(a), np.sin(a)] for a in angles]) * (length / (2 * np.sin(np.pi / sides)))
+def fractal_triangle(order, length=1.0):
+    angles = np.linspace(0, 2 * np.pi, 4)
+    points = np.array([[np.cos(a), np.sin(a)] for a in angles]) * (length / (2 * np.sin(np.pi / 3)))
 
-    def generate_cutout(pA, pB, sides):
-        pA_to_B = pB - pA
-        segment_length = np.linalg.norm([pA_to_B[0], pA_to_B[1]])
-        cutout_vertices = [pA]
-        angle = np.arctan2(pA_to_B[1], pA_to_B[0]) + np.pi / sides
-
-        for i in range(sides):
-            vertex = cutout_vertices[-1] + np.array([segment_length * np.cos(angle), segment_length * np.sin(angle)])
-            cutout_vertices.append(vertex)
-            angle += 2 * np.pi / sides
-
-        cutout_vertices.append(pB)
-        return np.array(cutout_vertices)
-
-    def subdivide_polygon(points, sides):
+    def subdivide_triangle(points):
         new_points = []
         for i in range(len(points) - 1):
             p1 = points[i]
             p2 = points[i + 1]
             pA = p1 + (p2 - p1) / 3
             pB = p1 + 2 * (p2 - p1) / 3
-            cutout = generate_cutout(pA, pB, sides)
-            new_points.append(p1)
-            new_points.extend(cutout)
+            dx, dy = pB - pA
+            length = np.linalg.norm(pB - pA)
+            angle = np.arctan2(dy, dx) + np.pi / 3
+            pC = pA + np.array([length * np.cos(angle), length * np.sin(angle)])
+            new_points.extend([p1, pA, pC, pB])
         new_points.append(points[-1])
-        return new_points
+        return np.array(new_points)
 
     for _ in range(order):
-        points = subdivide_polygon(points, sides)
+        points = subdivide_triangle(points)
 
-    return np.array(points)
+    return points
 
 order = 3
-sides = 5
 length = 1.0
 
-points = fractal_polygon(order, sides, length)
+points = fractal_triangle(order, length)
 
 plt.figure(figsize=(8, 8))
 plt.plot(points[:, 0], points[:, 1], 'b-')
 plt.axis('equal')
-plt.title(f"Fractal Polygon with {sides} sides (Order {order})")
+plt.title(f"Fractal Triangle (Order {order})")
 plt.show()
